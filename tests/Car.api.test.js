d0797a0883b545e1deff83477c952848c5813159
Atha Ardisa <ardisaatha1@gmail.com>
2022-11-20 23:18:20 +0700

    add testing for cars api

--- tests/Car.api.test.js
+++ tests/Car.api.test.js
@@ -6,44 +6,61 @@ const app = require('../app/index');
 
 dotenv.config();
 
-// describe("Dog", () => {
-//     it("should have name called 'Arnold'", () => {
-//         const dog = new Dog("Arnold");
-
-//         expect(dog).toHaveProperty("name", "Arnold");
-//     });
-
-//     it("should be able to bark and return 'Woof!'", () => {
-//         const dog = new Dog("Arnold");
-//         expect(dog.bark()).toEqual("Woof!");
-//     });
-// });
-
-// describe("API get all cars", () => {
-//     it("success get all data cars", (done) => {
-//         request(app)
-//             .get("/v1/cars")
-//             .expect(200, done);
-//     });
-// });
-
+// get all car test
 describe('API get all cars', () => {
   it('success get all data cars', async () => {
     const response = await request(app).get('/v1/cars');
     expect(response.statusCode).toBe(200);
   });
 });
-
+// get car by id test
 describe('API get car By ID', () => {
   it('success get data car', async () => {
     const response = await request(app).get('/v1/cars/20');
     expect(response.statusCode).toBe(200);
   });
 });
-
+// delete car test
 describe('API delete car by ID', () => {
   it('Unauthorized', async () => {
     const response = await request(app).delete('/v1/cars/20');
+    expect(response.statusCode).toBe(401);
+  });
+});
+// update car test
+describe('API update car by ID', () => {
+  it('Unauthorized', async () => {
+    const cars = {
+      name: 'string',
+      price: 0,
+      image: 'string',
+      size: 'string',
+    };
+    const response = await request(app).put('/v1/cars/20').send(cars);
+    expect(response.statusCode).toBe(401);
+  });
+});
+// create car test
+describe('API create cars', () => {
+  it('invalid token', async () => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwibmFtZSI6InN0cmluZyIsImVtYWlsIjoic3RyaW5nIiwiaW1hZ2UiOm51bGwsInJvbGUiOnsiaWQiOjEsIm5hbWUiOiJDVVNUT01FUiJ9LCJpYXQiOjE2NjgwMTg4MjB9.weFZDBPqqS0dA7RPNzlytJln8bZXBs0cfJPlAvzKVtw';
+    const cars = {
+      name: 'string',
+      price: 0,
+      image: 'string',
+      size: 'string',
+    };
+    const response = await request(app).post('/v1/cars').set('Authorization', `Bearer ${token}`).send(cars);
+    expect(response.statusCode).toBe(401);
+  });
+  it('Unauthorized', async () => {
+    const cars = {
+      name: 'string',
+      price: 0,
+      image: 'string',
+      size: 'string',
+    };
+    const response = await request(app).post('/v1/cars').send(cars);
     expect(response.statusCode).toBe(401);
   });
 });
